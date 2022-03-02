f8f4de56659b0c7d63b3c4038c105c1dbe2ce1d8
KURT ORIOQUE <kurtorioque112@gmail.com>
2022-03-03 01:10:44 +0800

    new config

---
+++ src/config/firebase/index.tsx
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAYV4wwoI082x60vojtk6NPlxs4lSqIifk",
+    authDomain: "simple-order-64158.firebaseapp.com",
+    projectId: "simple-order-64158",
+    storageBucket: "simple-order-64158.appspot.com",
+    messagingSenderId: "1048365821396",
+    appId: "1:1048365821396:web:25898ae97831d2adb9ba41"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
