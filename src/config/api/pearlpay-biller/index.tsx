2a15ce0d918977c29c9dc8c13b04f61bab303767
KURT ORIOQUE <kurtorioque112@gmail.com>
2022-03-22 01:56:12 +0800

    add a new version

--- src/config/api/pearlpay-biller/index.tsx
+++ src/config/api/pearlpay-biller/index.tsx
@@ -1,11 +1,7 @@
-import axios, {AxiosProxyConfig} from 'axios'
-import sha1 from 'sha1'
+import axios from 'axios'
 import {Description} from "../../../types"
 
-const url:string = process.env.REACT_APP_base_url || 'https://pgi-stg-ws.pearlpay.io/api/v1/transactions/generate',
-      token:string = process.env.x_pearlPay_Token || '08919f7b1e1299f74feb4309ab388d12e6e868ae',
-      code:string = process.env.x_pearlPay_Code || 'PEARLPAY_TEST_BILLER'
-
+const url = process.env.NODE_ENV === 'development'? 'http://localhost:3001': 'https://simple-order-backend-pearlpay.herokuapp.com'
 export const payRequestPearlPay = async (amount: Number,
              tnxid: String,
              name: String,
@@ -14,39 +10,27 @@ export const payRequestPearlPay = async (amount: Number,
              description: Description,
              redirect_url:  URL) => {
 
-    // createProxyMiddleware({target: 'https://pgi-stg-ws.pearlpay.io/api/v1/transactions/generate',changeOrigin: true})
-
-
-    const corsHeaders =  {
-        'Access-Control-Allow-Headers': ['X-PearlPay-Token','X-PearlPay-Code'],
-        'Access-Control-Allow-Methods': 'POST',
-        'Access-Control-Allow-Origin': 'https://pgi-stg-ws.pearlpay.io',
-        'X-PearlPay-Token': '08919f7b1e1299f74feb4309ab388d12e6e868ae',
-        'X-PearlPay-Code': 'PEARLPAY_TEST_BILLER'
-    };
-
     let formData = new FormData();
-    const sha = await sha1(`${amount}${tnxid}08919f7b1e1299f74feb4309ab388d12e6e868ae`)
     formData.append('amount', String(amount));
     formData.append('txnid',String(tnxid));
     formData.append('callback_url','https://simple-order-64158.web.app/');
-    formData.append('digest',sha);
     formData.append('name',String(name));
     formData.append('mobile',String(mobile));
     formData.append('description',String(description));
     formData.append('redirect_url',String(redirect_url));
 
-    await axios.post(url,formData,{
-        headers:{
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Headers': '*',
-            'Content-Type':'application/json',
-            'X-PearlPay-Code': code,
-            'X-PearlPay-Token': token
+    await axios.post(url+'/pay',{
+        data: {
+            amount:  String(amount),
+            txnid: String(tnxid),
+            callback_url: 'https://simple-order-64158.web.app/',
+            name: String(name),
+            mobile: String(mobile),
+            description: String(description),
+            redirect_url: String(redirect_url)
         }
     }).then(response => {
-        const {url} = response.data.data;
-
+        const {url} = response.data;
         window.open(url);
     }).catch(error => {
         console.log("hotodog")
